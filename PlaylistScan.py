1a701066f4cbeed42f0c807589bf6dcc498f28c4
ArthurBeaulieu <contact@abeaulieu.fr>
2021-01-28 18:33:13 +0100

    improved py scanning tool

--- PlaylistScan.py
+++ PlaylistScan.py
@@ -19,14 +19,15 @@ version = '0.0.1'
 def main():
     # Init argparse arguments
     ap = argparse.ArgumentParser()
-    ap.add_argument('folder', help='The input folder path to crawl (absolute or relative)')
+    ap.add_argument('folder', help='The input folder path to crawl (absolute or relative)', nargs='+')
     # Main modes
     ap.add_argument('-s', '--scan', help='Scan a folder to create a JSON file for web UI', action='store_true')
     ap.add_argument('-m', '--minify', help='Minifies the JSON output to maximize performances', action='store_true')
     args = vars(ap.parse_args())
+    folder = ' '.join(args['folder'])
     # Preventing path from missing its trailing slash (or backslash for win compatibility)
-    if not args['folder'].endswith('\\') and not args['folder'].endswith('/'):
-        print('  The path \'{}\' is invalid...'.format(args['folder']))
+    if not folder.endswith('\\') and not folder.endswith('/')  and not folder.endswith('"'):
+        print('  The path \'{}\' is invalid...'.format(folder))
         print('  It must end with a / or an \\, depending on your system\n')
         print('> Exiting PlaylistScan.py')
         sys.exit(-1)
@@ -46,19 +47,20 @@ def main():
 
 # Will crawl the folder path given in argument, and all its sub-directories
 def scanFolder(args):
-    folderName = os.path.basename(os.path.normpath(args['folder']))
+    folderPath = ' '.join(args['folder'])
+    folderName = os.path.basename(os.path.normpath(folderPath))
     # First make a copy of the playlist in website assets, to be easily accessible
     if os.path.exists('./assets/playlist/{}'.format(folderName)):
         print('> Removing previous copy of playlist from assets')
         shutil.rmtree('./assets/playlist/{}'.format(folderName))
     print('> Starting playlist copy in Meax assets folder\n')
-    shutil.copytree(args['folder'], './assets/playlist/{}'.format(folderName), copy_function=logCopytree)
+    shutil.copytree(folderPath, './assets/playlist/{}'.format(folderName), copy_function=logCopytree)
 
     print('\n> Generating JSON output')
     output = {}
     output['tracks'] = []
     # Sort directories so they are handled in the alphabetical order
-    for root, directories, files in sorted(os.walk(args['folder'])):
+    for root, directories, files in sorted(os.walk(folderPath)):
         files = [f for f in files if not f[0] == '.'] # Ignore hidden files
         directories[:] = [d for d in directories if not d[0] == '.'] # ignore hidden directories
         # Split root into an array of folders
@@ -76,13 +78,32 @@ def scanFolder(args):
                 trackObject['url'] = './assets/playlist/{}/{}'.format(folderName, file)
                 if extension == '.mp3':
                     audioTag = ID3(preservedPath + '{}'.format(file))
-                    self._fillFromMP3()
+                    if 'TIT2' in audioTag and audioTag['TIT2'].text[0] != '':
+                        trackObject['title'] = audioTag['TIT2'].text[0].rstrip()
+                    if 'TPE1' in audioTag:
+                        trackObject['artist'] = audioTag['TPE1'].text[0]
+                    if 'TCOM' in audioTag and audioTag['TCOM'].text[0] != '':
+                        trackObject['composer'] = audioTag['TCOM'].text[0].rstrip()
+                    if 'TCON' in audioTag and audioTag['TCON'].text[0] != '':
+                        trackObject['genre'] = audioTag['TCON'].text[0].rstrip()
+                    if 'TDRC' in audioTag and audioTag['TDRC'].text[0].get_text() != '':
+                        trackObject['date'] = audioTag['TDRC'].text[0].get_text()[:4].rstrip()
+                    if 'TBPM' in audioTag and audioTag['TBPM'].text[0] != '':
+                        trackObject['bpm'] = audioTag['TBPM'].text[0].rstrip()
+                    if 'TKEY' in audioTag and audioTag['TBPM'].text[0] != '':
+                        trackObject['key'] = audioTag['TKEY'].text[0].rstrip()
                 elif extension == '.flac':
                     audioTag = FLAC(preservedPath + '{}'.format(file))
                     if 'TITLE' in audioTag:
                         trackObject['title'] = audioTag['TITLE'][0]
                     if 'ARTIST' in audioTag:
                         trackObject['artist'] = audioTag['ARTIST'][0]
+                    if 'COMPOSER' in audioTag:
+                        trackObject['composer'] = audioTag['COMPOSER'][0]
+                    if 'GENRE' in audioTag:
+                        trackObject['genre'] = audioTag['GENRE'][0]
+                    if 'DATE' in audioTag:
+                        trackObject['date'] = audioTag['DATE'][0]
                     if 'BPM' in audioTag:
                         trackObject['bpm'] = audioTag['BPM'][0]
                     if 'INITIALKEY' in audioTag:
